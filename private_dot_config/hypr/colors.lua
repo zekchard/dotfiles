4a3611d585db0067eb992863ea6b8814438b1d1c
zekchard <janmaurick@gmail.com>
2026-06-02 18:12:04 +0800

    Update .config/btop/themes/matugen.theme Update .config/fastfetch/wriorindeffcolored.png Update .config/gtk-4.0/colors.css Update .config/hypr/colors.conf Update .config/hypr/colors.lua Update .config/kitty/matugen.conf Update .config/matugen/config.toml Update .config/matugen/current-color Update .config/matugen/templates/prismlauncher.json Update .config/micro/colorschemes/matugen.micro Update .config/gtk-3.0/colors.css Update .config/pulse-visualizer/themes/matugen.txt Update .config/swaync/colors.css Update .config/waybar/colors.css Update .config/wofi/colors.css

--- private_dot_config/hypr/colors.lua
+++ private_dot_config/hypr/colors.lua
@@ -1,101 +1,101 @@
 
-background = "rgba(0f1512ee)"
+background = "rgba(161217ee)"
 
 error = "rgba(ffb4abee)"
 
 error_container = "rgba(93000aee)"
 
-inverse_on_surface = "rgba(2c322eee)"
+inverse_on_surface = "rgba(332f35ee)"
 
-inverse_primary = "rgba(1d6b50ee)"
+inverse_primary = "rgba(735187ee)"
 
-inverse_surface = "rgba(dee4dfee)"
+inverse_surface = "rgba(e9e0e7ee)"
 
-on_background = "rgba(dee4dfee)"
+on_background = "rgba(e9e0e7ee)"
 
 on_error = "rgba(690005ee)"
 
 on_error_container = "rgba(ffdad6ee)"
 
-on_primary = "rgba(003827ee)"
+on_primary = "rgba(422356ee)"
 
-on_primary_container = "rgba(a7f2d0ee)"
+on_primary_container = "rgba(f4d9ffee)"
 
-on_primary_fixed = "rgba(002115ee)"
+on_primary_fixed = "rgba(2b0b3fee)"
 
-on_primary_fixed_variant = "rgba(00513aee)"
+on_primary_fixed_variant = "rgba(5a396eee)"
 
-on_secondary = "rgba(1f352bee)"
+on_secondary = "rgba(382c3eee)"
 
-on_secondary_container = "rgba(cfe9daee)"
+on_secondary_container = "rgba(efdcf5ee)"
 
-on_secondary_fixed = "rgba(092017ee)"
+on_secondary_fixed = "rgba(221729ee)"
 
-on_secondary_fixed_variant = "rgba(354b41ee)"
+on_secondary_fixed_variant = "rgba(4f4255ee)"
 
-on_surface = "rgba(dee4dfee)"
+on_surface = "rgba(e9e0e7ee)"
 
-on_surface_variant = "rgba(bfc9c2ee)"
+on_surface_variant = "rgba(cdc3ceee)"
 
-on_tertiary = "rgba(093544ee)"
+on_tertiary = "rgba(4c2527ee)"
 
-on_tertiary_container = "rgba(c1e8fcee)"
+on_tertiary_container = "rgba(ffdadaee)"
 
-on_tertiary_fixed = "rgba(001f2aee)"
+on_tertiary_fixed = "rgba(331013ee)"
 
-on_tertiary_fixed_variant = "rgba(254b5bee)"
+on_tertiary_fixed_variant = "rgba(663b3dee)"
 
-outline = "rgba(89938dee)"
+outline = "rgba(978e98ee)"
 
-outline_variant = "rgba(404944ee)"
+outline_variant = "rgba(4b454dee)"
 
-primary = "rgba(8bd5b5ee)"
+primary = "rgba(e0b8f6ee)"
 
-primary_container = "rgba(00513aee)"
+primary_container = "rgba(5a396eee)"
 
-primary_fixed = "rgba(a7f2d0ee)"
+primary_fixed = "rgba(f4d9ffee)"
 
-primary_fixed_dim = "rgba(8bd5b5ee)"
+primary_fixed_dim = "rgba(e0b8f6ee)"
 
 scrim = "rgba(000000ee)"
 
-secondary = "rgba(b3ccbeee)"
+secondary = "rgba(d2c1d8ee)"
 
-secondary_container = "rgba(354b41ee)"
+secondary_container = "rgba(4f4255ee)"
 
-secondary_fixed = "rgba(cfe9daee)"
+secondary_fixed = "rgba(efdcf5ee)"
 
-secondary_fixed_dim = "rgba(b3ccbeee)"
+secondary_fixed_dim = "rgba(d2c1d8ee)"
 
 shadow = "rgba(000000ee)"
 
-source_color = "rgba(c0fadeee)"
+source_color = "rgba(b797c8ee)"
 
-surface = "rgba(0f1512ee)"
+surface = "rgba(161217ee)"
 
-surface_bright = "rgba(343b37ee)"
+surface_bright = "rgba(3c383dee)"
 
-surface_container = "rgba(1b211eee)"
+surface_container = "rgba(221e24ee)"
 
-surface_container_high = "rgba(252b28ee)"
+surface_container_high = "rgba(2d282eee)"
 
-surface_container_highest = "rgba(303633ee)"
+surface_container_highest = "rgba(383339ee)"
 
-surface_container_low = "rgba(171d1aee)"
+surface_container_low = "rgba(1e1a20ee)"
 
-surface_container_lowest = "rgba(0a0f0dee)"
+surface_container_lowest = "rgba(100d12ee)"
 
-surface_dim = "rgba(0f1512ee)"
+surface_dim = "rgba(161217ee)"
 
-surface_tint = "rgba(8bd5b5ee)"
+surface_tint = "rgba(e0b8f6ee)"
 
-surface_variant = "rgba(404944ee)"
+surface_variant = "rgba(4b454dee)"
 
-tertiary = "rgba(a6ccdfee)"
+tertiary = "rgba(f4b7b9ee)"
 
-tertiary_container = "rgba(254b5bee)"
+tertiary_container = "rgba(663b3dee)"
 
-tertiary_fixed = "rgba(c1e8fcee)"
+tertiary_fixed = "rgba(ffdadaee)"
 
-tertiary_fixed_dim = "rgba(a6ccdfee)"
+tertiary_fixed_dim = "rgba(f4b7b9ee)"
 
