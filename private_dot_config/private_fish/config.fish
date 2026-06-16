326aa824dc66a2b96487ef53469e27c26e7c58d3
zekchard <janmaurick@gmail.com>
2026-06-16 10:30:15 +0800

    Update .config/btop/btop.conf Update .config/fish/config.fish Update .config/swaync/style.css Update .config/wofi/colors.css Update scripts/shipfetch/assets/acsw.png Update scripts/shipfetch/assets/agth.png Update scripts/shipfetch/assets/ayec.png Update scripts/shipfetch/assets/bnl.png Update scripts/shipfetch/assets/cfl.png Update scripts/shipfetch/assets/cstd.png Update scripts/shipfetch/assets/cylu.png Update scripts/shipfetch/assets/emk.png Update scripts/shipfetch/assets/hkvh.png Update scripts/shipfetch/assets/hrmy.png Update scripts/shipfetch/assets/htm.png Update scripts/shipfetch/assets/itsr.png Update scripts/shipfetch/assets/jlc.png Update scripts/shipfetch/assets/kzsc.png Update scripts/shipfetch/assets/nacl.png Update scripts/shipfetch/assets/phrn.png Update scripts/shipfetch/assets/rbh.png Update scripts/shipfetch/assets/rtrn.png Update scripts/shipfetch/assets/sdbn.png Update scripts/shipfetch/assets/sdbn2.png Update scripts/shipfetch/assets/stjn.png Update scripts/shipfetch/assets/wrnd.png Update scripts/shipfetch/assets/xvn.png Update scripts/shipfetch/assets/xyn.png Update scripts/shipfetch/assets/ynt.png

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
 cat ~/.cache/matugen/sequences.txt 2> /dev/null
+set -gx fish_greeting ' '
 
 # Created by `pipx` on 2025-03-15 20:43:40
 set -gx PATH "$PATH:/home/zek/.local/bin"
@@ -11,8 +12,6 @@ set -gx PATH "$PATH:/home/zek/.cargo/bin"
 
 fish_add_path /home/zek/.spicetify
 
-set -gx fish_greeting ' '
-
 # autostart
 # fastfetch -c /home/zek/wriorinde.jsonc --logo-recache
 $HOME/scripts/shipfetch/shipfetch.sh
@@ -21,17 +20,30 @@ starship init fish | source
 # aliases
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias configdotfish='$EDITOR /home/zek/.config/fish/config.fish'
+alias configdotfish='$EDITOR ~/.config/fish/config.fish'
 alias eimikosfetch='fastfetch -c ~/eimikosff3.jsonc'
 alias wriorindefetch='fastfetch -c /home/zek/wriorinde.jsonc --logo-recache'
+
 alias screenrec="/home/zek/scripts/screenrec.sh"
+
 alias hyprconf='yazi ~/.config/hypr/'
 alias wbref='killall -SIGUSR2 waybar'
 alias wbconf='yazi ~/.config/waybar'
-alias xterm='xterm -fg white -bg black'
-alias tuifi="env tuifi_theme=azawy tuitilities_default_background=True tuifi_vim_mode=True tuifi_show_hidden=True tuifi_ctrl_scroll_sensitivity=14 tuifi_scroll_sensitivity=7 tuifi"
+
 alias cl="matugen --dry-run --verbose --show-colors --continue-on-error -r gaussian -t scheme-tonal-spot color hex $(cat ~/.config/matugen/current-color)"
 alias cr="matugen --verbose --show-colors --continue-on-error -r gaussian -t scheme-tonal-spot color hex $(cat ~/.config/matugen/current-color)"
+
+alias psyu='eos-update'
+alias psy='sudo pacman -Sy'
+alias psyy='sudo pacman -Syy'
+
+alias syu='eos-update'
+alias sy='sudo pacman -Sy'
+alias syy='sudo pacman -Syy'
+
+alias ysyu='eos-update --yay'
+alias ysy='yay -Sy'
+alias ysyy='yay -Syy'
 
 end
 
