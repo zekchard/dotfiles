67c0bedb3da6c30ca8b53bfafdbe1426364708db
zekchard <janmaurick@gmail.com>
2026-07-27 10:11:13 +0800

    Update .config/swaync/gamemodewatch.sh

--- private_dot_config/swaync/executable_gamemodewatch.sh
+++ private_dot_config/swaync/executable_gamemodewatch.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env sh
-if [ "$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')" = 1 ] ; then
-echo true
-else
-echo false
-fi
+echo $(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
